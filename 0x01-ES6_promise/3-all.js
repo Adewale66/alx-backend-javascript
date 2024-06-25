4041e90d87982f4e5ef9c1899fc59b5541bc99c5
adewalekujore <adewalekujore6@gmail.com>
2024-06-25 22:06:09 +0100

    task 2

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((photo) => {
-    createUser().then((data) => {
-      console.log(`${photo.body} ${data.firstName} ${data.lastName}`);
-    });
-  });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
